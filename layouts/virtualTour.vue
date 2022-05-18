56245f8ca5ed242389e87d0960493699d721232f
Naash <dgmg@outlook.fr>
2022-05-18 13:56:06 +0200

    💩✨ Add virtualTour

---
+++ layouts/virtualTour.vue
@@ -0,0 +1,23 @@
+<template>
+  <div data-theme="bumblebee" class="bg-base-200 min-h-screen">
+    <div class="h-screen flex flex-col bg-base-200">
+      <NavBar />
+      <div class="h-full">
+        <Nuxt keep-alive class="bg-base-200 h-full" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+/* Hide scrollbar for Chrome, Safari and Opera */
+.container-snap::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.container-snap {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+</style>
