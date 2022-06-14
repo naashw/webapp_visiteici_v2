668aa3f44803a7ff102bbe70fefe0c946cf2c79b
Naash <dgmg@outlook.fr>
2022-06-14 21:02:37 +0200

    ♻️ Refactor code
    Refactor and adapt code for api change

--- pages/annonces/_id.vue
+++ pages/annonces/_id.vue
@@ -9,16 +9,16 @@
           <h1 class="capitalize text-2xl">{{ annonce.nom }}</h1>
 
           <div class="flex gap-2 text-slate-400">
-            <p>{{ annonce.nb_pieces }} Pièces</p>
+            <p>{{ annonce.appartement.nb_pieces }} Pièces</p>
             <span>-</span>
-            <p>{{ annonce.surface.split(".")[0] }}m²</p>
+            <p>{{ annonce.appartement.surface.split(".")[0] }}m²</p>
             <span>-</span>
-            <p>{{ annonce.ville }} {{ annonce.code_postal }}</p>
+            <p>{{ annonce.appartement.ville }} {{ annonce.code_postal }}</p>
           </div>
 
           <div>
             <p class="text-lg font-semibold">
-              {{ annonce.prix.split(".")[0] }}€
+              {{ annonce.appartement.prix.split(".")[0] }}€
               <span class="font-normal" v-if="annonce.charges_comprises">charges comprises</span>
               <span class="font-normal" v-else>hors charges</span>
             </p>
@@ -39,23 +39,23 @@
 
           <h2>Critères</h2>
           <div class="grid grid-cols-4 gap-2">
-            <AnnoncesCriteresBadge :critere="annonce.meublé" name="meublé" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.meublé" name="meublé" />
             <AnnoncesCriteresBadge
-              :critere="annonce.fibre_optique"
+              :critere="annonce.appartement.fibre_optique"
               name="fibre optique"
             />
-            <AnnoncesCriteresBadge :critere="annonce.balcon" name="balcon" />
-            <AnnoncesCriteresBadge :critere="annonce.terrasse" name="terrasse" />
-            <AnnoncesCriteresBadge :critere="annonce.cave" name="cave" />
-            <AnnoncesCriteresBadge :critere="annonce.jardin" name="jardin" />
-            <AnnoncesCriteresBadge :critere="annonce.parking" name="parking" />
-            <AnnoncesCriteresBadge :critere="annonce.garage" name="garage" />
-            <AnnoncesCriteresBadge :critere="annonce.ascenseur" name="ascenseur" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.balcon" name="balcon" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.terrasse" name="terrasse" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.cave" name="cave" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.jardin" name="jardin" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.parking" name="parking" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.garage" name="garage" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.ascenseur" name="ascenseur" />
             <AnnoncesCriteresBadge
-              :critere="annonce.classe_energie"
+              :critere="annonce.appartement.classe_energie"
               name="classe energie"
             />
-            <AnnoncesCriteresBadge :critere="annonce.GES" name="GES" />
+            <AnnoncesCriteresBadge :critere="annonce.appartement.GES" name="GES" />
           </div>
         </div>
         <div>
@@ -72,17 +72,16 @@ export default {
       
     const annonce = await $axios
       .get(`/api/annonces/${params.id}`)
-      .then((res) => res.data[0]);
-
-      annonce.photos = annonce.photos !== null ? annonce.photos.split(',') : [];
+      .then((res) => res.data);
 
       const user = await $axios
       .get(`/api/userPublic/${annonce.user_id}`)
       .then((res) => res.data.user); 
 
     return { annonce, user };
+
     } catch (error) {
-      
+      console.log(error);
       redirect('/annonces/');
     }
   },
